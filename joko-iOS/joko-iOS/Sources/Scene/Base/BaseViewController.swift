68011c51bfcec49c60e20f51fd97d424df6c3f60
jihoon <leejh080628@naver.com>
2025-07-14 19:16:52 +0900

    setting :: baseVM 추가

--- joko-iOS/joko-iOS/Sources/Scene/Base/BaseViewController.swift
+++ joko-iOS/joko-iOS/Sources/Scene/Base/BaseViewController.swift
@@ -1,12 +1,56 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
-open class BaseViewController: UIViewController {
+open class BaseViewController<ViewModel: BaseViewModel>: UIViewController, UIGestureRecognizerDelegate
+{
+    public let disposeBag = DisposeBag()
+    public var viewModel: ViewModel
 
+    public var viewWillAppearRelay = PublishRelay<Void>()
+    
+    
+    public init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        attribute()
+        bind()
+    }
+    
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        addView()
+        setLayout()
+    }
+    
+    
+    open func attribute() {
+        view.backgroundColor = .systemBackground
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
 
+    
+    open func bindAction() {
+        // Rx 액션을 설정하는 함수
+    }
+    open func bind() {
+        // UI 바인딩을 설정하는 함수
+    }
+    open func addView() {
+        // 서브뷰를 구성하는 함수
+    }
+    open func setLayout() {
+        // 레이아웃을 설정하는 함수
+    }
 
 }
 

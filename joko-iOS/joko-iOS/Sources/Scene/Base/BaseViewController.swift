7c10fc2f1ca1d7848db6533a34e43ca9cd94e9bd
jihoon <leejh080628@naver.com>
2025-07-14 23:46:11 +0900

    chore :: 주석

--- joko-iOS/joko-iOS/Sources/Scene/Base/BaseViewController.swift
+++ joko-iOS/joko-iOS/Sources/Scene/Base/BaseViewController.swift
@@ -17,28 +17,22 @@ open class BaseViewController<ViewModel: BaseViewModel>: UIViewController, UIGes
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    
     open override func viewDidLoad() {
         super.viewDidLoad()
         attribute()
         bind()
     }
-    
     open override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         addView()
         setLayout()
     }
-    
-    
+
     open func attribute() {
         view.backgroundColor = .systemBackground
         navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
 
-    
     open func bindAction() {
         // Rx 액션을 설정하는 함수
     }
@@ -51,6 +45,5 @@ open class BaseViewController<ViewModel: BaseViewModel>: UIViewController, UIGes
     open func setLayout() {
         // 레이아웃을 설정하는 함수
     }
-
 }
 

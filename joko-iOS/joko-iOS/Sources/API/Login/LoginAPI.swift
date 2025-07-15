b8f91f8fa46fd62ba2a7627588f965406497bb43
jihoon <leejh080628@naver.com>
2025-07-15 12:22:16 +0900

    feat :: loginAPI 추가

---
+++ joko-iOS/joko-iOS/Sources/API/Login/LoginAPI.swift
@@ -0,0 +1,40 @@
+import Moya
+import Foundation
+
+enum LoginAPI {
+    case login(accountId: String, password: String)
+}
+
+extension LoginAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://172.20.10.2:8080")!
+    }
+
+    var path: String {
+        switch self {
+        case .login:
+            return "/auth/login"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .login:
+            return .post
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case let .login(accountId, password):
+            return .requestParameters(
+                parameters: ["accountId": accountId, "password": password],
+                encoding: JSONEncoding.default
+            )
+        }
+    }
+
+    var headers: [String: String]? {
+        return ["Content-Type": "application/json"]
+    }
+}

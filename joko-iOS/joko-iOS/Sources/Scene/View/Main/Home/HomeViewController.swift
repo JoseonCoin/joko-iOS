633d6e657770b0022a65f9058c80c5f261be9dfa
jihoon <leejh080628@naver.com>
2025-07-16 00:35:26 +0900

    feat :: 글씨체 추가 및 홈 배경

--- joko-iOS/joko-iOS/Sources/Scene/View/Main/Home/HomeViewController.swift
+++ joko-iOS/joko-iOS/Sources/Scene/View/Main/Home/HomeViewController.swift
@@ -3,13 +3,28 @@ import SnapKit
 import Then
 
 final class HomeViewController: BaseViewController<HomeViewModel> {
-    private let topImageView = UIView().then {
+    private let topSkyView = UIView().then {
         $0.backgroundColor = .skyblue
     }
-    
+    private let cloudImageView = UIImageView().then {
+        $0.image = UIImage(named: "cloud")
+        $0.contentMode = .scaleAspectFit
+        $0.tintColor = .white1
+    }
+    private let cloudImageView2 = UIImageView().then {
+        $0.image = UIImage(named: "cloud")
+        $0.contentMode = .scaleAspectFit
+        $0.tintColor = .white1
+    }
+    private let bottomBackView = UIView().then {
+        $0.backgroundColor = .yellow1
+    }
     public override func addView() {
         [
-            topImageView
+            topSkyView,
+            cloudImageView,
+            cloudImageView2,
+            bottomBackView
         ].forEach { view.addSubview($0) }
     }
     
@@ -19,12 +34,27 @@ final class HomeViewController: BaseViewController<HomeViewModel> {
     }
     
     public override func setLayout() {
-        topImageView.snp.makeConstraints {
+        topSkyView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.bottom.equalToSuperview().inset(329)
         }
-
+        bottomBackView.snp.makeConstraints {
+            $0.top.equalTo(topSkyView.snp.bottom)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        cloudImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(119.04)
+            $0.leading.equalTo(54.79)
+            $0.width.equalTo(120)
+            $0.height.equalTo(58.25)
+        }
+        cloudImageView2.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(74.29)
+            $0.trailing.equalToSuperview().inset(51.75)
+        }
     }
 }
